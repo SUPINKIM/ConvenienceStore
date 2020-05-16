54051507b721fdf91fe2204d62d634c5208a2f92
SUPINKIM <49034615+SUPINKIM@users.noreply.github.com>
2020-05-17 01:35:43 +0900

    첫 번째 화면 수정 - 원하는 카테고리 먼저 설정하여 저장
    메인 화면에 저장한 카테고리의 상품이 보일 수 있도록 함

--- main.dart
+++ main.dart
@@ -23,10 +23,114 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Firstpage(),
+      home: //Firstpage(),
+      SelectCategory(),
     );
   }
 }
+
+class SelectCategory extends StatefulWidget {
+  @override
+  _SelectCategoryState createState() => _SelectCategoryState();
+}
+
+class _SelectCategoryState extends State<SelectCategory> {
+
+  List category = [];
+  bool _buttonEnabled1 = false; // 각 버튼을 처음에는 비활성화 함.
+  bool _buttonEnabled2 = false;
+  bool _buttonEnabled3 = false;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 160.0, 10.0, 60.0),//EdgeInsets.all (20.0),
+            ),
+            Text('관심 있는 상품 카테고리를 선택해 주세요.'),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('음료'),
+                  onPressed: () {
+                    print(_buttonEnabled1);
+                    print("음료");
+                    setState(() {
+                      _buttonEnabled1 = !_buttonEnabled1; // true <-> false
+                      sumCount(_buttonEnabled1);
+                    });
+                    print(_buttonEnabled1);
+                  },
+                ),
+                RaisedButton(
+                  child: Text('즉석 조리'),
+                  onPressed: () {
+                    category.add('즉석 조리');
+                    setState(() {
+                      _buttonEnabled2 = !_buttonEnabled2; // true <-> false
+                      sumCount(_buttonEnabled2);
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text('과자'),
+                  onPressed: (){
+                    category.add('과자');
+                    setState(() {
+                      _buttonEnabled3 = !_buttonEnabled3; // true <-> false
+                      sumCount(_buttonEnabled3);
+                    });
+                    print(category);
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('간편 식사'),
+                ),
+                RaisedButton(
+                  child: Text('아이스크림'),
+                ),
+                RaisedButton(
+                  child: Text('생활용품'),
+                ),
+              ],
+            ),
+            RaisedButton(
+              child: Text('저장'),
+              onPressed: count==0 ? null : (){
+                setState(() {
+                  goFirstpage();
+                });
+              },
+            ),
+          ],
+        ),
+    );
+  }
+
+  void sumCount(bool b){
+    if (b){count += 1;}
+    else {count-=1;}
+  }
+
+  void goFirstpage(){
+    Navigator.push(
+        context,  // 기본 파라미터, SecondRoute로 전달
+        MaterialPageRoute(builder: (context)=>Firstpage())
+    );
+  }
+
+}
+
+
 
 class Firstpage extends StatefulWidget {
   @override
